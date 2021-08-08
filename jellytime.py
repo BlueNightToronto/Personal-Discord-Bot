e308dd62aed6dc809f35d8c2c3e0a4d551b21d9e
BlueNightToronto <35881143+BlueNightToronto@users.noreply.github.com>
2021-08-08 09:40:21 -0400

    Add files via upload

---
+++ jellytime.py
@@ -0,0 +1,17 @@
+import discord
+from discord.ext import commands
+
+
+class jellytime(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    @commands.command()
+    async def jellytime(self, ctx):
+        embed_name = 'Jelly Time'
+        embed = discord.Embed(color=0x1e405d)
+        embed.add_field(name=embed_name,
+                        value='Do you know what time it is. Jelly Time!'
+                        inline=False)
+        embed.set_image(url='')
+        await ctx.send(embed=embed)
